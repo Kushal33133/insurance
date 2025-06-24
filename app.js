307ad794c55160f4bbf4d77dc39be3764f2dcc0e
Kushal <kushalbhakhandwal@gmail.com>
2025-06-24 21:17:56 +0530

    local storage implementation

--- app.js
+++ app.js
@@ -4,3 +4,26 @@
   menuToggle.addEventListener('click', () => {
     navbar.classList.toggle('show');
   });
+
+
+  document.getElementById("getInTouch").addEventListener("click", function (event) {
+    event.preventDefault(); 
+
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+
+
+    userData = {
+        name:name,
+        email:email,
+        phone:phone
+    }
+
+    console.log(userData);
+    localStorage.setItem("userData",JSON.stringify(userData))
+    alert("Thank you for getting in touch, " + userData.name + "! We will get back to you soon at " + userData.email + ".");
+
+  });
+
